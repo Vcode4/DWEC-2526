e7bb0d0dba72d5223f0a7e54ce888f6290c7e481
Vcode4 <127629218+Vcode4@users.noreply.github.com>
2025-10-20 10:41:25 +0200

    Add files via upload
    Ejercicio Trump- Funciones, document.getElementById, css

---
+++ trump_ejercicio/trump.js
@@ -0,0 +1,17 @@
+
+
+
+function apareceTrump(){
+     var mostrar = document.getElementById('imagen');
+
+    mostrar.style.opacity="100%";       
+            
+        }
+
+ function ocultaTrump(){
+    var oculto = document.getElementById('imagen');
+
+    oculto.style.opacity="0%";
+        }
+
+
